aa07995d226f15f127f9622182d5f937f1216723
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2024-06-27 17:16:58 +0700

    FEAT[29]:chang sth

--- models/report.js
+++ models/report.js
@@ -4,18 +4,22 @@ const { ObjectId } = mongoose.Schema
 const reportSchema = new mongoose.Schema(
   {
     objectId: {
+      // ID của product hoặc store bị báo cáo
       type: String,
       required: true
     },
     isStore: {
+      // nếu là true thì là báo cáo shop, còn false là báo cáo product
       type: Boolean,
       required: true
     },
     reason: {
+      // lý do báo cáo
       type: String,
       required: true
     },
     reportBy: {
+      // id của người báo cáo
       type: ObjectId,
       ref: 'User',
       required: true
