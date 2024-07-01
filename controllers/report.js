f64a48f392f2cd4d2974e71f84ce4f2b8d50dc7f
19520032 <cuongsdbt@gmail.com>
2024-07-01 19:56:13 +0700

    report api

--- controllers/report.js
+++ controllers/report.js
@@ -1,32 +1,90 @@
-const Report = require('../models/report')
-const Notification = require('../models/notification')
+const Report = require("../models/report");
+const Notification = require("../models/notification");
+const {
+  sendReportShopEmail,
+  sendReportProductEmail,
+} = require("../controllers/email");
+const Store = require("../models/store");
+const Product = require("../models/product");
 
-exports.reportShop = async (req, res) => {
+exports.getReport = async (req, res) => {
   try {
-    const { objectId, isStore, reason, reportBy } = req.body
+    const reports = await Report.find();
+
+    res.status(200).json({ reports: reports });
+  } catch (error) {
+    res.status(500).json({ message: "Lỗi server", error });
+  }
+};
+
+exports.deleteReport = async (req, res) => {
+  try {
+    await Report.deleteOne({ _id: req.params.id });
+
+    res.status(200).json({ message: "Delete successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Lỗi server", error });
+  }
+};
+
+exports.report = async (req, res) => {
+  try {
+    const { objectId, isStore, reason, reportBy } = req.body;
     const report = new Report({
       objectId,
       isStore,
       reason,
-      reportBy
-    })
+      reportBy,
+    });
 
-    await report.save()
+    await report.save();
 
     // Gửi thông báo cho admin
-    const adminId = process.env.ADMIN_ID
+    const adminId = process.env.ADMIN_ID;
     const adminNotification = new Notification({
       message: `Có báo cáo mới: ${reason}`,
       userId: adminId,
       isRead: false,
-      orderId: objectId
-    })
+      orderId: "",
+    });
 
-    await adminNotification.save()
+    await adminNotification.save();
 
-    res.status(201).json({ message: 'Báo cáo đã được gửi' })
+    // if (isStore) {
+    //   const store = await Store.findById(objectId);
+
+    //   if (store) {
+    //     await sendReportShopEmail(
+    //       {
+    //         params: {
+    //           userId: store.ownerId,
+    //         },
+    //       },
+    //       res
+    //     );
+    //   }
+    // } else {
+    //   const product = await Product.findById(objectId);
+
+    //   if (product) {
+    //     const store = await Store.findById(product.storeId);
+
+    //     if (store) {
+    //       await sendReportProductEmail(
+    //         {
+    //           params: {
+    //             userId: store.ownerId,
+    //           },
+    //         },
+    //         res
+    //       );
+    //     }
+    //   }
+    // }
+
+    res.status(201).json({ message: "Báo cáo đã được gửi" });
   } catch (error) {
-    console.error('Error in reportShop:', error)
-    res.status(500).json({ message: 'Lỗi server', error })
+    console.error("Error in reportShop:", error);
+    res.status(500).json({ message: "Lỗi server", error });
   }
-}
+};
--- routes/notification.js
+++ routes/notification.js
@@ -1,21 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
   getNotifications,
   updateRead,
-  deleteNotifications
-} = require('../controllers/notification')
+  deleteNotifications,
+} = require("../controllers/notification");
 const {
   sendBanStoreEmail,
-  sendActiveStoreEmail
-} = require('../controllers/email')
-const { isAuth } = require('../controllers/auth')
+  sendActiveStoreEmail,
+  sendBanProductEmail,
+  sendActiveProductEmail,
+} = require("../controllers/email");
+const { isAuth } = require("../controllers/auth");
 
-router.get('/notification/:userId', getNotifications)
-router.put('/notification/:userId', updateRead)
-router.delete('/notification/:userId', deleteNotifications)
-router.post('/send-ban-store/:userId', sendBanStoreEmail)
-router.post('/send-active-store/:userId', sendActiveStoreEmail)
+router.get("/notification/:userId", getNotifications);
+router.put("/notification/:userId", updateRead);
+router.delete("/notification/:userId", deleteNotifications);
+router.post("/send-ban-store/:userId", sendBanStoreEmail);
+router.post("/send-active-store/:userId", sendActiveStoreEmail);
 
-module.exports = router
+router.post("/send-ban-product/:userId", sendBanProductEmail);
+router.post("/send-active-product/:userId", sendActiveProductEmail);
+
+module.exports = router;
