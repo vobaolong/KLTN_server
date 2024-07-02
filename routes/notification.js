2f8e1bd185ee72fddefe37e46ccc2745a9b616b6
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2024-07-03 00:16:52 +0700

    Update notification.js

--- routes/notification.js
+++ routes/notification.js
@@ -1,26 +1,21 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const {
   getNotifications,
   updateRead,
-  deleteNotifications,
-} = require("../controllers/notification");
+  deleteNotifications
+} = require('../controllers/notification')
 const {
   sendBanStoreEmail,
-  sendActiveStoreEmail,
-  sendBanProductEmail,
-  sendActiveProductEmail,
-} = require("../controllers/email");
-const { isAuth } = require("../controllers/auth");
+  sendActiveStoreEmail
+} = require('../controllers/email')
+const { isAuth } = require('../controllers/auth')
 
-router.get("/notification/:userId", getNotifications);
-router.put("/notification/:userId", updateRead);
-router.delete("/notification/:userId", deleteNotifications);
-router.post("/send-ban-store/:userId", sendBanStoreEmail);
-router.post("/send-active-store/:userId", sendActiveStoreEmail);
+router.get('/notification/:userId', getNotifications)
+router.put('/notification/:userId', updateRead)
+router.delete('/notification/:userId', deleteNotifications)
+router.post('/send-ban-store/:userId', sendBanStoreEmail)
+router.post('/send-active-store/:userId', sendActiveStoreEmail)
 
-router.post("/send-ban-product/:userId", sendBanProductEmail);
-router.post("/send-active-product/:userId", sendActiveProductEmail);
-
-module.exports = router;
+module.exports = router
